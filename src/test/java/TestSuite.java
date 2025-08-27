c4316d0f3a792e6c656e573cf9cc93937208ecba
kotteletfisk <ostekugle@gmail.com>
2025-08-27 12:57:30 +0200

    pom add sqlite

--- src/test/java/TestSuite.java
+++ src/test/java/TestSuite.java
@@ -1,15 +1,27 @@
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.kotteletfisk.todo_exer.Other_class;
 
+@DisplayNameGeneration(DisplayNameGenerator.Simple.class)
 class TestSuite {
 
-       @Test
-       public void test1() {
+    @Test
+    @DisplayName("Custom test name")
+    public void test1() {
         Other_class oc = new Other_class();
         int actual = oc.add(2, 3);
         int expected = 5;
         assertEquals(actual, expected);
         System.out.println("Test executed");
     }
+
+    @Test
+    public void lol() { 
+
+    }
+
 }
